7e04b16cc07ce79710bbb4c50d633dcd8203c204
officialchiragparmar <chiragparmar12177@gmail.com>
2020-02-13 02:20:39 +0530

    Remove MiuiCamera & Add GoogleCamera
    This version of GoogleCamera works on all whyred variants and all firmwares.
    GCam_7.2.010_Burial-v8.1_release29.01.20
    taken from @orgesified
    
    Signed-off-by: officialchiragparmar <chiragparmar12177@gmail.com>

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap
+    GoogleCamera
 
 # CNE
 PRODUCT_PACKAGES += \
